db47a07570239b21b569552bc33a7a37b069fde3
Mathias <n9clip@n9clip-System-Product-Name.(none)>
2012-09-06 14:28:19 +0300

    pushi

--- src/dungeon/duugne/Peli.java
+++ src/dungeon/duugne/Peli.java
@@ -2,9 +2,27 @@ package dungeon.duugne;
 
 import dungeon.peli.Alusta;
 import dungeon.peli.Pelaaja;
+import dungeon.peli.Ratkaisija;
 
 public class Peli {
-    public Peli(Alusta alusta, Pelaaja pelaaja1, Pelaaja pelaaja2){
-        
+    
+    Pelaaja pelaaja1;
+    Pelaaja pelaaja2;
+    Alusta alusta;
+    Ratkaisija ratkaisija;
+    
+    public Peli(Alusta alusta, Ratkaisija ratkaisija, Pelaaja pelaaja1, Pelaaja pelaaja2){
+        this.alusta = alusta;
+        this.ratkaisija = ratkaisija;
+        this.pelaaja1 = pelaaja1;
+        this.pelaaja2 = pelaaja2;
+    }
+    
+    public void aloita(){
+        alusta.tyhjaaLauta();
+        while(ratkaisija.tarkistaVoitto(1, 1)) {
+            pelaaja1.seuraava_siirto(alusta);
+            pelaaja2.seuraava_siirto(alusta);
+        }
     }
 }
