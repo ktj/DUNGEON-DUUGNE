adad18daf64e6f614acdac5eeabb0730a0d29a7c
Mathias <n9clip@n9clip-System-Product-Name>
2012-09-25 14:09:46 +0300

    minmax moniajo, mikä loppuen lopuksi ei oo nopeampi, mutta ehkä  voi parantaa.. dunno..

--- Dungeen/src/main/java/main/DUNGEONDUUGNE.java
+++ Dungeen/src/main/java/main/DUNGEONDUUGNE.java
@@ -8,52 +8,57 @@ import logiikka.Ratkaisija;
 import tekoaly.*;
 
 public class DUNGEONDUUGNE {
-
+    
     public static void main(String[] args) {
-
+        
         Alusta alusta = new Alusta(5, 5);
         Ratkaisija ratkaisija = new Ratkaisija();
         Tekstikayttoliittyma tekstikäli = new Tekstikayttoliittyma();
         Pelaaja pelaaja1 = new Ihmispelaaja(tekstikäli);
-        Pelaaja pelaaja2 = new Tekoaly(new MinMax(ratkaisija,new Arvioija()), new Puukasaaja(new Arvioija()));
-
+        Pelaaja pelaaja2 = new Tekoaly(new MinMax(ratkaisija, new Arvioija()), new Puukasaaja(new Arvioija()));
+        
         Peli peli = new Peli(alusta, ratkaisija, pelaaja2, pelaaja2);
         peli.aloita();
 
 
 
 
-//
+////
 //        alusta.lisaaMerkkiLaudalle(1, 0, 'x');
 //        alusta.lisaaMerkkiLaudalle(2, 0, 'x');
-//        alusta.lisaaMerkkiLaudalle(2, 1, 'x');
-//        alusta.lisaaMerkkiLaudalle(0, 1, 'o');
+////        alusta.lisaaMerkkiLaudalle(2, 1, 'x');
+////        alusta.lisaaMerkkiLaudalle(0, 1, 'o');
 //        alusta.lisaaMerkkiLaudalle(1, 1, 'o');
 //        alusta.lisaaMerkkiLaudalle(1, 2, 'o');
 //        
-       
-        
-//        Arvioija arvioija = new Arvioija();
-//        System.out.println(arvioija.arvioiAlusta(alusta));
 //
-//        alusta.lisaaMerkkiLaudalle(2, 2, 'o');
-//        System.out.println(arvioija.arvioiAlusta(alusta));
-        
-//        pelaaja2.seuraava_siirto(alusta, 'x').tulostaAlusta();
-
 //
+////        Arvioija arvioija = new Arvioija();
+////        System.out.println(arvioija.arvioiAlusta(alusta));
+////
+////        alusta.lisaaMerkkiLaudalle(2, 2, 'o');
+////        System.out.println(arvioija.arvioiAlusta(alusta));
+//
+////        pelaaja2.seuraava_siirto(alusta, 'x').tulostaAlusta();
+//
+//        alusta.tulostaAlusta();
 //        Puukasaaja kasaaja = new Puukasaaja(new Arvioija());
 //        Puu puu = kasaaja.kasaaPuu(alusta, 'o', 100);
-//        tulostaPuu(puu);
+////        tulostaPuu(puu);
+////        
+////        alusta.tulostaAlusta();
+////        
+//        MinMax minmax = new MinMax(new Ratkaisija(), new Arvioija());
 //        
-//        alusta.tulostaAlusta();
+//        Alusta alusta2 = minmax.aloitaMin(puu);
+//        alusta2.tulostaAlusta();
 //        
-//        MinMax minmax = new MinMax(new Ratkaisija());
-//        
-//        minmax.aloitaMin(puu, 3).tulostaAlusta();
-
+//        puu = kasaaja.kasaaPuu(alusta2, 'x', 100);
+//        minmax.aloitaMax(puu).tulostaAlusta();
+////        
+        
     }
-
+    
     private static void tulostaPuu(Puu puu) {
         puu.haeAlusta().tulostaAlusta();
         System.out.println("-------");
--- Dungeen/src/main/java/tekoaly/MinMax.java
+++ Dungeen/src/main/java/tekoaly/MinMax.java
@@ -1,5 +1,10 @@
 package tekoaly;
 
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import logiikka.Alusta;
 import logiikka.Ratkaisija;
 
@@ -13,11 +18,140 @@ public class MinMax {
         this.arvioija = arvioija;
     }
 
+    private class Tuple<X, Y> {
+
+        public final X x;
+        public final Y y;
+
+        public Tuple(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public Alusta aloitaMax(Puu puu) {
         Alusta alusta = puu.haeAlusta();
         if (onkovalmis(alusta)) {
             return alusta;
         }
+//        return moniajoMax(puu);
+        return yksiajoMax(puu);
+    }
+
+    public Alusta aloitaMin(Puu puu) {
+        Alusta alusta = puu.haeAlusta();
+        if (onkovalmis(alusta)) {
+            return alusta;
+        }
+//        return moniajoMin(puu);
+        return yksiajoMin(puu);
+    }
+
+    private Alusta moniajoMin(Puu puu) throws RuntimeException {
+        int threads = Runtime.getRuntime().availableProcessors();
+        ExecutorService service = Executors.newFixedThreadPool(threads);
+        ArrayList<Future<Tuple<Integer, Alusta>>> futures = new ArrayList<Future<Tuple<Integer, Alusta>>>();
+        for (final Puu lapsi : puu.haeLapset()) {
+            Callable<Tuple<Integer, Alusta>> callable = new Callable<Tuple<Integer, Alusta>>() {
+
+                public Tuple<Integer, Alusta> call() throws Exception {
+                    int arvo = maksimi(lapsi, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                    return new Tuple<Integer, Alusta>(arvo, lapsi.haeAlusta());
+                }
+            };
+
+            futures.add(service.submit(callable));
+        }
+        service.shutdown();
+
+        Tuple<Integer, Alusta> tuple = null;
+        Alusta pienin = puu.haeAlusta();
+        int v = Integer.MAX_VALUE;
+        int lapsenarvo;
+
+        for (Future<Tuple<Integer, Alusta>> lapsi : futures) {
+            try {
+                tuple = lapsi.get();
+            } catch (Exception ex) {
+                throw new RuntimeException("Moniajos vikaa!");
+            }
+            lapsenarvo = tuple.x;
+            if (lapsenarvo < v) {
+                pienin = tuple.y;
+                v = lapsenarvo;
+            }
+            if (lapsenarvo == Integer.MIN_VALUE) {
+                break;
+            }
+        }
+        return pienin;
+    }
+
+    private Alusta moniajoMax(Puu puu) throws RuntimeException {
+        int threads = Runtime.getRuntime().availableProcessors();
+        ExecutorService service = Executors.newFixedThreadPool(threads);
+
+        ArrayList<Future<Tuple<Integer, Alusta>>> futures = new ArrayList<Future<Tuple<Integer, Alusta>>>();
+        for (final Puu lapsi : puu.haeLapset()) {
+            Callable<Tuple<Integer, Alusta>> callable = new Callable<Tuple<Integer, Alusta>>() {
+
+                public Tuple<Integer, Alusta> call() throws Exception {
+                    int arvo = minimi(lapsi, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                    return new Tuple<Integer, Alusta>(arvo, lapsi.haeAlusta());
+                }
+            };
+
+            futures.add(service.submit(callable));
+        }
+        service.shutdown();
+
+        Tuple<Integer, Alusta> tuple = null;
+        Alusta suurin = puu.haeAlusta();
+        int v = Integer.MIN_VALUE;
+        int lapsenarvo;
+
+        for (Future<Tuple<Integer, Alusta>> lapsi : futures) {
+            try {
+                tuple = lapsi.get();
+            } catch (Exception ex) {
+                throw new RuntimeException("Moniajos vikaa!");
+            }
+            lapsenarvo = tuple.x;
+            if (lapsenarvo > v) {
+                suurin = tuple.y;
+                v = lapsenarvo;
+            }
+            if (lapsenarvo == Integer.MAX_VALUE) {
+                break;
+            }
+        }
+
+        return suurin;
+    }
+
+    private Alusta yksiajoMin(Puu puu) {
+        Alusta pienin = puu.haeAlusta();
+        int v = Integer.MAX_VALUE;
+        int lapsenarvo;
+
+        int alfa = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
+
+        for (Puu lapsi : puu.haeLapset()) {
+            lapsenarvo = maksimi(lapsi, alfa, beta);
+            if (lapsenarvo < v) {
+                pienin = lapsi.haeAlusta();
+                v = lapsenarvo;
+            }
+            if (lapsenarvo == Integer.MIN_VALUE) {
+                break;
+            }
+            beta = Math.min(beta, v);
+        }
+        return pienin;
+    }
+
+    private Alusta yksiajoMax(Puu puu) {
         Alusta suurin = puu.haeAlusta();
         int v = Integer.MIN_VALUE;
         int lapsenarvo;
@@ -27,7 +161,7 @@ public class MinMax {
 
         for (Puu lapsi : puu.haeLapset()) {
             lapsenarvo = minimi(lapsi, alfa, beta);
-            if (lapsenarvo >= v) {
+            if (lapsenarvo > v) {
                 suurin = lapsi.haeAlusta();
                 v = lapsenarvo;
             }
@@ -39,59 +173,28 @@ public class MinMax {
         return suurin;
     }
 
-    public Alusta aloitaMin(Puu puu) {
-        Alusta alusta = puu.haeAlusta();
-        if (onkovalmis(alusta)) {
-            return alusta;
-        }
-        Alusta pienin = puu.haeAlusta();
-        int v = Integer.MAX_VALUE;
-        int lapsenarvo;
-
-        int alfa = Integer.MIN_VALUE;
-        int beta = Integer.MAX_VALUE;
-        
-        for (Puu lapsi : puu.haeLapset()) {
-            lapsenarvo = maksimi(lapsi, alfa, beta);
-            if (lapsenarvo <= v) {
-                pienin = lapsi.haeAlusta();
-                v = lapsenarvo;
-            }
-            if (lapsenarvo == Integer.MIN_VALUE) {
-                break;
-            }
-            beta = Math.max(beta, v);
-        }
-        return pienin;
-    }
-
     private boolean onkovalmis(Alusta alusta) {
         int ratkaisuarvo = ratkaisija.etsiVoitto(alusta);
-        if (alusta.onkoLautaTaynna() || ratkaisuarvo != 0) {
-            return true;
-        }
-        return false;
+        return (alusta.onkoLautaTaynna() || ratkaisuarvo != 0);
     }
 
     private int maksimi(Puu puu, int alfa, int beta) {
         Alusta alusta = puu.haeAlusta();
         int ratkaisuarvo = ratkaisija.etsiVoitto(alusta);
-        if (alusta.onkoLautaTaynna() || ratkaisuarvo != 0) {
+        if (onkovalmis(alusta)) {
             return ratkaisuarvo;
         }
         int v = Integer.MIN_VALUE;
         for (Puu lapsi : puu.haeLapset()) {
             v = Math.max(v, minimi(lapsi, alfa, beta));
-            if (v >= beta || v == Integer.MAX_VALUE) {
+            if (v >= beta) {
                 return v;
             }
             alfa = Math.max(alfa, v);
         }
-
-        if (v == Integer.MIN_VALUE) {
+        if (puu.haeLapset().isEmpty()) {
             v = alusta.getArvio();
         }
-
         return v;
     }
 
@@ -104,12 +207,12 @@ public class MinMax {
         int v = Integer.MAX_VALUE;
         for (Puu lapsi : puu.haeLapset()) {
             v = Math.min(v, maksimi(lapsi, alfa, beta));
-            if (v <= alfa || v == Integer.MIN_VALUE) {
+            if (v <= alfa) {
                 return v;
             }
             beta = Math.min(beta, v);
         }
-        if (v == Integer.MAX_VALUE) {
+        if (puu.haeLapset().isEmpty()) {
             v = alusta.getArvio();
         }
         return v;
